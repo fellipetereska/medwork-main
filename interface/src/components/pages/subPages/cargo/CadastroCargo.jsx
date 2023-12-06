1ab913224f139b7d1a20216c8d41558036aecd4a
tereska <fellipetereska@gmail.com>
2023-12-06 09:23:19 -0300

    testar vercel

--- interface/src/components/pages/subPages/cargo/CadastroCargo.jsx
+++ interface/src/components/pages/subPages/cargo/CadastroCargo.jsx
@@ -1,93 +1,125 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import axios from 'axios';
 import { Link } from 'react-router-dom'
 
 import Back from '../../../layout/Back'
+import SearchInput from "../components/SearchInput";
 import FrmCadastroCargo from "./frmCadastroCargo";
 import GridCadastroCargo from "./gridCadastroCargo";
+import { supabase } from "../../../../services/api";
 
 function CadastroCargo() {
 
-    // Instanciando e Definindo como vazio
-    const [data, setData] = useState(null);
-    const [empresa, setEmpresa] = useState([]);
-    const [onEdit, setOnEdit] = useState(null);
-    const [formData, setFormData] = useState({
-        nome_empresa: '',
-        razao_social: '',
-        cnpj: '',
-        endereco: '',
-        cidade: '',
-        contato: '',
-        telefone: ''
-    });
+  // Instanciando e Definindo como vazio
+  const [cargo, setCargo] = useState([]);
+  const [setor, setSetor] = useState([]);
+  const [setorFind, setSetorFind] = useState([]);
+  const [setorNome, setSetorNome] = useState(null);
+  const [onEdit, setOnEdit] = useState(null);
 
-    //Instanciando Modal
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [editData, setEditData] = useState(null);
+  //Instanciando o Search
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCargo, setFilteredCargo] = useState([]);
 
-    const openModal = (data) => {
-        setIsEditModalOpen(true);
-        setEditData(data)
-    };
-
-
-    const handleSave = () => {
-        console.log("Dados Salvos", formData);
-    };
-
-    // Pegando os dados do banco
-    const getEmpresa = async () => {
-        try {
-            const res = await axios.get("http://localhost:8800/empresa");
-            setEmpresa(res.data.sort((a, b) => (a.nome_empresa > b.nome_empresa ? 1 : -1)));
-        } catch (error) {
-            toast.error(error);
+  // Pegando os dados do banco
+  const getCargo = async () => {
+    try {
+      const { data } = await supabase.from("cargo").select();
+      const sortData = data.sort((a, b) => {
+        if (a.ativo !== b.ativo) {
+          return a.ativo ? -1 : 1;
         }
-    };
 
-    useEffect(() => {
-        getEmpresa();
-    }, []); // Chama apenas uma vez quando o componente é montado
+        return a.nome_cargo.localeCompare(b.nome_cargo);
+      })
+      setCargo(sortData);
+    } catch (error) {
+      toast.error(error);
+    }
+  };
 
-    //Funções do Modal
-    const handleEditModalOpen = (data) => {
-        setIsEditModalOpen(true);
-        setEditData(data);
-    };
+  const getSetor = async () => {
+    try {
+      const { data } = await supabase.from("setor").select();
+      setSetor(data);
+    } catch (error) {
+      toast.error(error);
+    }
+  };
 
-    const handleCancelEdit = () => {
-        setEditData(null);
-        setIsEditModalOpen(false);
+  useEffect(() => {
+    getCargo();
+    getSetor();
+  }, []);
+
+  const findSetor = (fkSetorId) => {
+    if(!setor) {
+      return 'N/A';
     }
 
-    const handleEdit = (selectedEmpresa) => {
-        setOnEdit(selectedEmpresa)
-    };
+    const setores = setor.find((c) => c.id_setor === fkSetorId);
+    return setores ? setores.nome_setor : 'N/A';
+  }
+
+  const handleEdit = (selectedSetor) => {
+    setOnEdit(selectedSetor)
+
+    if (selectedSetor.fk_setor_id) {
+      const setorInfo = setor.find((c) => c.id_setor === selectedSetor.fk_setor_id)
+      if (setorInfo) {
+        setSetorNome(setorInfo.nome_setor);
+      }
+    }
+  };
+
+  //Função para Pesquisa
+  useEffect(() => {
+    const filtered = cargo.filter((carg) => carg.nome_cargo.toLowerCase().includes(searchTerm.toLowerCase()));
+    setFilteredCargo(filtered);
+  }, [searchTerm, cargo]);
 
 
-    return (
-        <div>
-            <div className="tab-content mt-14 mb-32">
-                <div>
+  const handleSearch = (term) => {
+    // Atualizar o estado do termo de pesquisa com o valor fornecido
+    setSearchTerm(term);
+  }
 
-                    <Link to="/cadastros">
-                        <Back />
-                    </Link>
-                    
-                    <FrmCadastroCargo onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getEmpresa} />
 
-                    <GridCadastroCargo
-                        empresa={empresa}
-                        setEmpresa={setEmpresa}
-                        setOnEdit={handleEdit}
-                        handleEditModalOpen={() => handleEditModalOpen(data)} // Chamando a função para abrir o modal
-                    />
-                </div>
-            </div>
+  return (
+    <div className="tab-content mt-14 mb-32">
+      <div className="flex justify-center items-center">
+        {/* Botão para voltar */}
+        <div className="absolute left-0">
+          <Link to="/cadastros">
+            <Back />
+          </Link>
         </div>
-    )
+        <h1 className="text-3xl font-extrabold text-sky-700">Cadastrar Cargo</h1>
+      </div>
+
+      <FrmCadastroCargo
+        onEdit={onEdit}
+        setOnEdit={setOnEdit}
+        getCargo={getCargo}
+        set={setorNome}
+      />
+
+      {/* Barra de pesquisa */}
+      <div className="flex justify-center w-full">
+        <div className="w-3/6">
+          <SearchInput onSearch={handleSearch} placeholder="Buscar Cargo..." />
+        </div>
+      </div>
+
+      {/* Tabela Empresa */}
+      <GridCadastroCargo
+        cargos={filteredCargo}
+        setCargo={setCargo}
+        setOnEdit={handleEdit}
+        find={findSetor}
+      />
+    </div>
+  )
 }
 
 export default CadastroCargo;
---
+++ interface/src/components/pages/subPages/components/Modal/ModalSearchSetor.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import SearchInput from '../SearchInput';
+
+const ModalSearchUnidadeEmpresa = ({ onCancel, isOpen, children, onContactSelect }) => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="modal-overlay absolute inset-0 backdrop-blur-[1px] bg-black bg-opacity-10" onClick={onCancel}></div>
+      <div className="modal-container max-w-lg bg-white mx-auto rounded-xl z-50 overflow-y-auto px-8 py-4 max-h-[80vh]">
+        <div className='flex justify-between items-center py-2'>
+          <h1 className='text-xl font-bold text-sky-800'>Selecione um Setor</h1>
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8"
+              onClick={onCancel}>
+              <svg className="flex m-auto w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className='border-b border-gray-200'></div>
+        <div className='flex justify-center items-center py-2'>
+          <p className='text-sm text-gray-500 text-center'>
+            Selecione o Setor para qual esse cargo pertence
+          </p>
+        </div>
+        <div className="flex justify-center w-full mt-2 mb-2">
+          <div className="w-5/6">
+            <SearchInput onSearch={handleSearch} placeholder="Buscar Empresa..." />
+          </div>
+        </div>
+        <ul className='space-y-3 py-3'>
+          {children
+            .filter((empresa) =>
+              empresa.nome_setor.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              empresa.ambiente_setor.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((empresa, i) => (
+              <li
+                key={i}
+                className="py-3 hover:bg-gray-100 hover:shadow-sm shadow-sm bg-gray-50 cursor-pointer px-4 rounded-md"
+                onClick={() => onContactSelect(empresa.id_setor, empresa.nome_setor)}
+              >
+                <div className="flex items-center gap-12">
+                  <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-700">
+                      {empresa.nome_setor}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                      {empresa.ambiente_setor}
+                    </p>
+                  </div>
+                  <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                    
+                  </div>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+export default ModalSearchUnidadeEmpresa;
