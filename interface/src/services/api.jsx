5494c8d187d152b09822953a3ec13a4990cec385
tereska <fellipetereska@gmail.com>
2024-01-12 15:09:28 -0300

    arrumando link connect /services/api

--- interface/src/services/api.jsx
+++ interface/src/services/api.jsx
@@ -8,4 +8,4 @@ export const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5la3ZvcGhpZ3N3enV0Ymt6aXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1ODMzODQsImV4cCI6MjAxNjE1OTM4NH0.SRvKTxAtIavc8uRY2hdkZEB-LInQ6PA5fpIMIOq_J7g"
 );
 
-export const connect = "https://https://medwork-api-git-master-fellipetereska.vercel.app";
+export const connect = "https://medwork-api.vercel.app/";
